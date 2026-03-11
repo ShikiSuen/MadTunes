6b715369489cd429a16d210ead08b6365d4e11a9
ShikiSuen <shikisuen@outlook.com>
2026-03-11 14:34:17 +0800

    Repo // Force copyright year to 2026.

--- SPMPackages/MadTunesSPM/Sources/MadTunesKit/Views/SubViews/AlbumGridItemView.swift
+++ SPMPackages/MadTunesSPM/Sources/MadTunesKit/Views/SubViews/AlbumGridItemView.swift
@@ -1,4 +1,4 @@
-// (c) 2025 and onwards Shiki Suen (AGPL v3.0 License or later).
+// (c) 2026 and onwards Shiki Suen (AGPL v3.0 License or later).
 // ====================
 // This code is released under the SPDX-License-Identifier: `AGPL-3.0-or-later`.
 
